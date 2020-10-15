d9bb8751bc0e1bc6fcb15419fa0d4f6b8a046cd4
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-15 17:59:32 +0530

    Tried notifs

--- public/worker.js
+++ public/worker.js
@@ -41,3 +41,44 @@ self.addEventListener("activate", (event) => {
     })
   );
 });
+
+Notification.requestPermission(function (status) {
+  console.log("Notification permission status:", status);
+  if (status === "granted") {
+    navigator.serviceWorker.getRegistration().then(function (reg) {
+      var options = {
+        body: "Here is a notification body!",
+
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1,
+        },
+        actions: [
+          {
+            action: "explore",
+            title: "Explore this new world",
+          },
+          {
+            action: "close",
+            title: "Close notification",
+          },
+        ],
+      };
+      reg.showNotification("Hello world!", options);
+    });
+  }
+});
+
+self.addEventListener("notificationclick", function (e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === "close") {
+    notification.close();
+  } else {
+    clients.openWindow("http://www.example.com");
+    notification.close();
+  }
+});
